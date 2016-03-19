52a407780a65223447d37469c861a6e1df727d4d
Adam Snodgrass <overra@gmail.com>
2016-03-19 00:07:34 -0500

    Rewrite for compatibility with Uint8Array

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 'use strict';
 
-var marker = Buffer('PK\x03\x04\x14\x00\x06\x00');
-
-module.exports = function (buffer) {
-  if (Buffer.isBuffer(buffer)) {
-    return buffer.slice(0,8).equals(marker);
+module.exports = function (buf) {
+  if (buf[0] === 0x50 && buf[1] === 0x4b && buf[2] === 0x03 && buf[3] === 0x04
+   && buf[4] === 0x14 && buf[5] === 0x00 && buf[6] === 0x06 && buf[7] === 0x00) {
+    return true;
   }
 
   return false;
